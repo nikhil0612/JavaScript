2ae8dbdcf8ba7020007d3be0cac63f40162f6050
Nikhil Soni <76102204+nikhil0612@users.noreply.github.com>
2024-01-23 17:42:48 +0000

    more operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -25,3 +25,31 @@ let someNumber = 33;
 let stringNumber = String(someNumber); // now we are converting number to string
 console.log(stringNumber);
 console.log(typeof stringNumber);
+// ----------------------------------Operations---------------------------------------------
+
+let value = 3;
+let negativeValue = -value;
+console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " Nikhil";
+let str3 = str1+str2;
+console.log(str3);
+// operation examples
+// console.log(1+2); // 3
+// console.log(1 + "2"); // 12
+// console.log("1" + 2); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+// console.log("1" + "2"); // 12
+
+console.log(+true); // 1;
+// console.log(true+); // its wrong and gives error
+// console.log(+""); // it will give 0 but no use and also no sense of this
