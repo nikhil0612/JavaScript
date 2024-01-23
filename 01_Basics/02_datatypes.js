6378c780df1568819909bf6c1e84cb79078526ac
Nikhil Soni <76102204+nikhil0612@users.noreply.github.com>
2024-01-23 14:19:51 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 5); // we are using nodejs, not browser
+
+console.log(3+3);
+
+let name = "Nikhil";
+let age = 19;
+let isLoggedIn = true;
+
+// DataTypes
+// number => 2 to power 53
+// bigInt => used in very large numbers
+// string => ""
+// boolean => true/false
+// null => standalone value or it is a empty representation of a value
+// undefined => it is just declared not defined
+// symbol => unique // mostly used in react to uniquely identify
+// object
+
+
+console.log(typeof null); // Object
+console.log(typeof undefined); // undefined
