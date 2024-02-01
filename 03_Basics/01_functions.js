d589c0b50f33b32a39f3370b67d4adafc7d021f7
Nikhil Soni <76102204+nikhil0612@users.noreply.github.com>
2024-02-01 07:22:43 +0000

    functions extended

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -53,5 +53,30 @@ const result = addTwoNumbers(5,7);
 }
 // console.log(loginUserMessage("Nikhil"));
 //console.log(loginUserMessage("")); // when we pass empty string then it returns nothing
-console.log(loginUserMessage()); // when we don't pass anything then it returns undefined
+// console.log(loginUserMessage()); // when we don't pass anything then it returns undefined
 
+//---------------------------funtions remaining-------------------------------------
+// rest operator is same as var-arg in java, most importantly rest operator should be at last like (val1, val2, ...val)
+function calcualteCartValue(...num1){ // in JS it is called rest operator and spread operator but their significance differ place to place
+    return num1;
+}
+// console.log(calcualteCartValue(200, 300, 500, 2000));
+
+// we can also pass object in functions
+const user = {
+    username: "Nikhil",
+    price: 200
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+handleObject(user);
+
+// we can also pass arrays in functions
+const arr = [200, 400, 500, 1000];
+function retrunSecondValue(getArray){
+    return getArray[1];
+}
+// console.log(retrunSecondValue(arr));
+console.log(retrunSecondValue([100, 500, 1000, 2000])); // we can also pass direct array without variable
