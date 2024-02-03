da2c083f6f26bfc48a9c454cd226ab592150d51b
Nikhil Soni <76102204+nikhil0612@users.noreply.github.com>
2024-02-03 08:20:16 +0000

    nested scope

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -14,3 +14,45 @@ if(true){
     console.log(c); // it will execute and give value 30 and it should not print according to the programming scope rules so ultimately we should not use the var
 
 // in browser's console the scope is different and in the node(terminal) it is different
+
+//--------------------------------------scopes extended--------------------------------------
+// DOM(documents Object Model)
+
+//nested scope
+function one(){
+    const username = "nikhil";
+
+    function two() {
+        const website = "Youtube";
+        // console.log(username); // nikhil
+    }
+    // console.log(website); // it gives error
+    two(); // it is called after one()
+}
+
+one(); // it is called first
+
+if(true){
+    const username = "nikhil06";
+    if(username === "nikhil06"){
+        const website = " Youtube";
+        // console.log(username+website);
+    }
+    // console.log(website); // can't access
+}
+// console.log(username); // can't access
+
+//-----------------------------------Important Concept-----------------------------------
+// these are the function declaration type 
+addOne(7); // we can call this function here before its declared
+function addOne(number) {
+    return number+1;
+}
+addOne(5); // its normal call
+
+addTwo(2); // it will give error because by this type of definition(declaration) we store value in variable and its called expression
+const addTwo = function(number){
+    return number+2;
+}
+addTwo(8); // its normal call
+//it is also called hoisting
